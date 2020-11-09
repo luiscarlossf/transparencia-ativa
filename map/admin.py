519c6219b1d6c5b4622a0f85639968a71239dcfe
Luis Carlos <luiscarlos.sf2017@gmail.com>
2020-11-09 16:47:50 -0300

    Close issue #6

--- map/admin.py
+++ map/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Cidade, Estatistica, Pagina, Processo
+from .models import Cidade, Estatistica, Pagina, Processo, UploadDados
 
 admin.site.register(Cidade)
 admin.site.register(Estatistica)
 admin.site.register(Pagina)
 admin.site.register(Processo)
+admin.site.register(UploadDados)
