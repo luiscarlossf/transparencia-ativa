4075659feaa6d0cab3086db5b42f21e07cf9f895
luiscarlossf <luiscarlos.sf2017@gmail.com>
2020-12-03 12:42:10 -0300

    Adiciona arquivo para testes

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase,
 
-# Create your tests here.
+# Testa se o upload do arquivo está sendo 
+# realizado corretamente
+
+
+#Testa se os dados foram corretamente adicionados
+#no banco de dados.
+
+
+
